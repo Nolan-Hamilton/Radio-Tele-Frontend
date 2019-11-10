d4fd0ef6587e1167b792c49b4d845dc8d8368eef
AlaskaJay <thatthinker@gmail.com>
2019-11-09 19:33:57 -0500

    Added src code for RTAstroAPI

---
+++ vue-front-end/src/assets/RTAstronomicalAPI/scr/RTAstronomicalAPI/SkyView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RTAstronomicalAPI.Controllers
+{
+    public class SkyView
+    {
+        public String filepath { get; set; }
+    }
+}
