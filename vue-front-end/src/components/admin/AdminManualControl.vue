1503b343133b427bb6607621009ebcd0ff3bfa98
AlaskaJay <thatthinker@gmail.com>
2019-11-18 21:32:46 -0500

    Added buttons and fields for view of manual control page

--- vue-front-end/src/components/admin/AdminManualControl.vue
+++ vue-front-end/src/components/admin/AdminManualControl.vue
@@ -2,25 +2,111 @@
     <div>
         <loading v-if="$store.state.isLoading"></loading>
         <div v-if="!$store.state.isLoading">
-            <!-- Text Fields (for numbers) -->
-                <!-- Right Asc Hours -->
-                <!-- Right Asc Minutes -->
-                <!-- Right Asc in Dec -->
-                <!-- Declination -->
-                <!-- Azimuth -->
-                <!-- Elevation -->
-            <!-- Button to update from fields -->
-            <!-- Dropdown to select update mode -->
-            <!-- Buttons to jog by Right Asc and Declination -->
-                <!-- H = left    -->
-                <!-- J = up      -->
-                <!-- K = down    -->
-                <!-- L = right   -->
-            <!-- Buttons to jog by Azimuth and Elevation -->
-                <!-- H = left    -->
-                <!-- J = up      -->
-                <!-- K = down    -->
-                <!-- L = right   -->
+            <v-card flat>
+                <v-card-title class="headline">Manual Control</v-card-title>
+                <v-container grid-list-xs fluid>
+                    <v-layout wrap>
+                    <!-- Text Fields (for numbers) -->
+                        <!-- Right Asc Hours -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Right Ascension by Hours & Minutes')"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.rightAscHours]"
+                            label="Right Ascension Hours"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                        <!-- Right Asc Minutes -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Right Ascension by Hours & Minutes')"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.rightAscMinutes]"
+                            label="Right Ascension Minutes"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                        <!-- Right Asc in Dec -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Right Ascension by Decimal Value')"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.azimuth]"
+                            label="Right Ascension Decimals"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                        <!-- Declination -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Right Ascension by Decimal Value' || mode === 'Right Ascension by Hours & Minutes' )"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.declination]"
+                            label="Declination"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                        <!-- Azimuth -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Azimuth & Elevation')"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.azimuth]"
+                            label="Azimuth"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                        <!-- Elevation -->
+                        <v-flex xs6 sm4>
+                            <v-text-field
+                            :disabled="!(mode === 'Azimuth & Elevation')"
+                            :validate-on-blur="true"
+                            color="blue darken-2"
+                            :rules="[rules.elevation]"
+                            label="Elevation"
+                            type="number"
+                            class="number"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                    <!-- Button to update from fields -->
+                    <v-card-actions>
+                        <v-btn flat @click="update">Send Command</v-btn>
+                        <v-btn flat @click="resetForm">Clear Form</v-btn>
+                        <v-btn flat @click="resetForm">Back</v-btn>
+                    </v-card-actions>
+                    <!-- Dropdown to select update mode -->
+                    <v-flex xs12 sm6>
+                        <v-select
+                        v-model="mode"
+                        :items="modes"
+                        color="blue darken-2"
+                        label="Mode"
+                        required
+                        ></v-select>
+                    </v-flex>
+                    <v-card-actions>
+                    <!-- Buttons to jog by Right Asc and Declination -->
+                        <v-btn flat @click="foo">Right Asc Neg</v-btn> <!-- H = left    -->
+                        <v-btn flat @click="foo">Declination Pos</v-btn> <!-- J = up      -->
+                        <v-btn flat @click="foo">Declination Neg</v-btn> <!-- K = down    -->
+                        <v-btn flat @click="foo">Right Asc Pos</v-btn> <!-- L = right   -->
+
+                        <v-spacer></v-spacer>
+
+                    <!-- Buttons to jog by Azimuth and Elevation -->
+                        <v-btn flat @click="foo">Azimuth Neg</v-btn> <!-- H = left    -->
+                        <v-btn flat @click="foo">Elevation Pos</v-btn> <!-- J = up      -->
+                        <v-btn flat @click="foo">Elevation Neg</v-btn> <!-- K = down    -->
+                        <v-btn flat @click="foo">Azimuth Pos</v-btn> <!-- L = right   -->
+                    </v-card-actions>
+                </v-container>
+            </v-card>
         </div>
     </div>
 </template>
@@ -45,19 +131,20 @@ export default {
 
             mode: null,
 
-            modes: {
+            modes: [
                 "Right Ascension by Hours & Minutes",
                 "Right Ascension by Decimal Value",
-                "Azimuth and Elevation"
-            },
+                "Azimuth & Elevation"
+            ],
 
             rules: {
                 rightAscHours: val => (val && val.toString().length > 0 && val < 24 && val >= 0) || 'Must be between 0 and 23 hours',
                 rightAscMinutes: val => (val && val.toString().length > 0 && val < 60 && val >= 0) || 'Must be between 0 and 59 minutes',
+                declination: val => (val && val.toString().length > 0 && val >= 0 && val < 360) || 'Must be between 0 and 360',
                 numRequired: val => (val && val.toString().length > 0 && val <=90 && val >= -90) || 'Must be between 90 and -90',
                 azimuth: val => (val && val.toString().length > 0 && val >= 0 && val < 360) || 'Must be between 0 and 360',
                 elevation: val => (val && val.toString().length > 0 && val >= 0 && val <= 90) || 'Must be between 0 and 90'
-            },
+            }
         }
     },
     methods:{
@@ -68,6 +155,9 @@ export default {
             // This method will use a TCP client to send commands to the telescope.
         }
         // Considering giving each button a seperate method, but also considering cutting down on the number of buttons
+    },
+    components: {
+        Loading
     }
 };
 </script>
--- vue-front-end/src/views/Administration.vue
+++ vue-front-end/src/views/Administration.vue
@@ -38,6 +38,10 @@
           Sensors
           <v-icon>rss_feed</v-icon>
         </v-tab>
+        <v-tab>
+          Manual Control
+          <v-icon>rss_feed</v-icon>
+        </v-tab>
         
         <v-tab-item>
           <admin-log></admin-log>
@@ -66,6 +70,10 @@
         <v-tab-item>
           <admin-sensor-data></admin-sensor-data>
         </v-tab-item>
+        
+        <v-tab-item>
+          <admin-manual-control></admin-manual-control>
+        </v-tab-item>
       </v-tabs>
 
   </div>
@@ -84,6 +92,7 @@ import CelestialBodies from '../components/admin/CelestialBodies.vue';
 import Loading from "../components/utility/Loading";
 import WeatherStation from "../components/admin/WeatherStation.vue"
 import AdminSensorData from '../components/admin/AdminSensorData.vue';
+import AdminManualControl from "../components/admin/AdminManualControl.vue"
 
  export default {
     title: "Radio Telescope 1.1.0",
@@ -118,7 +127,8 @@ import AdminSensorData from '../components/admin/AdminSensorData.vue';
         AdminAppointmentApproval,
         CelestialBodies,
         WeatherStation,
-        AdminSensorData
+        AdminSensorData,
+        AdminManualControl
     },
     mounted() {
       this.authenticate()
