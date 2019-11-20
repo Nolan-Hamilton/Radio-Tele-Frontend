17a896ab233da5f3653f2afc2c534226eb9494a7
AlaskaJay <thatthinker@gmail.com>
2019-11-20 01:36:45 -0500

    Added button functionality

--- vue-front-end/src/components/admin/AdminManualControl.vue
+++ vue-front-end/src/components/admin/AdminManualControl.vue
@@ -10,7 +10,8 @@
                         <!-- Right Asc Hours -->
                         <v-flex xs6 sm4>
                             <v-text-field
-                            :disabled="!(mode === 'Right Ascension by Hours & Minutes')"
+                            v-model="rightAscHours"
+                            :disabled="!(mode === 'Right Ascension by Hours and Minutes')"
                             :validate-on-blur="true"
                             color="blue darken-2"
                             :rules="[rules.rightAscHours]"
@@ -21,7 +22,8 @@
                         <!-- Right Asc Minutes -->
                         <v-flex xs6 sm4>
                             <v-text-field
-                            :disabled="!(mode === 'Right Ascension by Hours & Minutes')"
+                            v-model="rightAscMinutes"
+                            :disabled="!(mode === 'Right Ascension by Hours and Minutes')"
                             :validate-on-blur="true"
                             color="blue darken-2"
                             :rules="[rules.rightAscMinutes]"
@@ -32,6 +34,7 @@
                         <!-- Right Asc in Dec -->
                         <v-flex xs6 sm4>
                             <v-text-field
+                            v-model="rightAscDecimal"
                             :disabled="!(mode === 'Right Ascension by Decimal Value')"
                             :validate-on-blur="true"
                             color="blue darken-2"
@@ -43,7 +46,8 @@
                         <!-- Declination -->
                         <v-flex xs6 sm4>
                             <v-text-field
-                            :disabled="!(mode === 'Right Ascension by Decimal Value' || mode === 'Right Ascension by Hours & Minutes' )"
+                            v-model="declination"
+                            :disabled="!(mode === 'Right Ascension by Decimal Value' || mode === 'Right Ascension by Hours and Minutes' )"
                             :validate-on-blur="true"
                             color="blue darken-2"
                             :rules="[rules.declination]"
@@ -54,7 +58,8 @@
                         <!-- Azimuth -->
                         <v-flex xs6 sm4>
                             <v-text-field
-                            :disabled="!(mode === 'Azimuth & Elevation')"
+                            v-model="azimuth"
+                            :disabled="!(mode === 'Azimuth and Elevation')"
                             :validate-on-blur="true"
                             color="blue darken-2"
                             :rules="[rules.azimuth]"
@@ -65,7 +70,8 @@
                         <!-- Elevation -->
                         <v-flex xs6 sm4>
                             <v-text-field
-                            :disabled="!(mode === 'Azimuth & Elevation')"
+                            v-model="elevation"
+                            :disabled="!(mode === 'Azimuth and Elevation')"
                             :validate-on-blur="true"
                             color="blue darken-2"
                             :rules="[rules.elevation]"
@@ -76,9 +82,9 @@
                     </v-layout>
                     <!-- Button to update from fields -->
                     <v-card-actions>
-                        <v-btn flat @click="update">Send Command</v-btn>
-                        <v-btn flat @click="resetForm">Clear Form</v-btn>
-                        <v-btn flat @click="resetForm">Back</v-btn>
+                        <v-btn flat @click="update(0)">Send Command</v-btn>
+                        <v-btn flat @click="clearForm">Clear Form</v-btn>
+                        <v-btn flat @click="clearForm">Back</v-btn>
                     </v-card-actions>
                     <!-- Dropdown to select update mode -->
                     <v-flex xs12 sm6>
@@ -92,18 +98,18 @@
                     </v-flex>
                     <v-card-actions>
                     <!-- Buttons to jog by Right Asc and Declination -->
-                        <v-btn flat @click="foo">Right Asc Neg</v-btn> <!-- H = left    -->
-                        <v-btn flat @click="foo">Declination Pos</v-btn> <!-- J = up      -->
-                        <v-btn flat @click="foo">Declination Neg</v-btn> <!-- K = down    -->
-                        <v-btn flat @click="foo">Right Asc Pos</v-btn> <!-- L = right   -->
+                        <v-btn flat @click="update(1)">Right Asc Neg</v-btn> <!-- H = left    -->
+                        <v-btn flat @click="update(2)">Declination Pos</v-btn> <!-- J = up      -->
+                        <v-btn flat @click="update(3)">Declination Neg</v-btn> <!-- K = down    -->
+                        <v-btn flat @click="update(4)">Right Asc Pos</v-btn> <!-- L = right   -->
 
                         <v-spacer></v-spacer>
 
                     <!-- Buttons to jog by Azimuth and Elevation -->
-                        <v-btn flat @click="foo">Azimuth Neg</v-btn> <!-- H = left    -->
-                        <v-btn flat @click="foo">Elevation Pos</v-btn> <!-- J = up      -->
-                        <v-btn flat @click="foo">Elevation Neg</v-btn> <!-- K = down    -->
-                        <v-btn flat @click="foo">Azimuth Pos</v-btn> <!-- L = right   -->
+                        <v-btn flat @click="update(5)">Azimuth Neg</v-btn> <!-- H = left    -->
+                        <v-btn flat @click="update(6)">Elevation Pos</v-btn> <!-- J = up      -->
+                        <v-btn flat @click="update(7)">Elevation Neg</v-btn> <!-- K = down    -->
+                        <v-btn flat @click="update(8)">Azimuth Pos</v-btn> <!-- L = right   -->
                     </v-card-actions>
                 </v-container>
             </v-card>
@@ -127,14 +133,14 @@ export default {
             declination: null,
 
             azimuth: null,
-            elavation: null,
+            elevation: null,
 
             mode: null,
 
             modes: [
-                "Right Ascension by Hours & Minutes",
+                "Right Ascension by Hours and Minutes",
                 "Right Ascension by Decimal Value",
-                "Azimuth & Elevation"
+                "Azimuth and Elevation"
             ],
 
             rules: {
@@ -148,13 +154,95 @@ export default {
         }
     },
     methods:{
-        update(){
-            // This method will update the display to show the correct Right Asc, Declination, Azimuth, and Elevation.
+        clearForm(){
+            this.rightAscHours = null,
+            this.rightAscMinutes = null,
+            this.rightAscDecimal = null,
+            this.declination = null,
+
+            this.azimuth = null,
+            this.elevation = null,
+
+            this.mode = null;
+        },
+        updateRightAsc(){
+            if(this.mode === "Right Ascension by Decimal Value") {
+                var totalMinutes = this.rightAscDecimal * 4;
+                this.rightAscHours = ((totalMinutes - (totalMinutes % 60)) / 60);
+                this.rightAscMinutes = (totalMinutes % 60);
+            }
+            else if(this.mode === "Right Ascension by Hours and Minutes") {
+                this.rightAscDecimal = this.rightAscHours * 15 + this.rightAscMinutes * 0.25;
+            }
+        },
+        update(val){
+            console.log("Update pressed!");
+            console.log(val);
+
+            if(val == 0) {
+                this.submit();
+            } else if(val == 1) {
+                // Right Asc Neg
+                if(this.mode === "Right Ascension by Decimal Value") {
+                    this.rightAscDecimal = parseFloat(this.rightAscDecimal) - 1;
+                }
+                else if(this.mode === "Right Ascension by Hours and Minutes") {
+                    this.rightAscMinutes = parseInt(this.rightAscMinutes) - 1;
+                    if(this.rightAscMinutes < 0) {
+                        this.rightAscHours = parseInt(this.rightAscHours) - 1;
+                        this.rightAscMinutes = parseInt(this.rightAscMinutes) + 60;
+                    }
+                }
+            } else if(val == 2) {
+                // Declination Pos
+                if(this.mode === "Right Ascension by Decimal Value" || this.mode === "Right Ascension by Hours and Minutes") {
+                    this.declination = parseFloat(this.declination) + 1;
+                }
+            } else if(val == 3) {
+                // Declination Neg
+                if(this.mode === "Right Ascension by Decimal Value" || this.mode === "Right Ascension by Hours and Minutes") {
+                    this.declination = parseFloat(this.declination) - 1;
+                }
+            } else if(val == 4) {
+                // Right Asc Pos
+                if(this.mode === "Right Ascension by Decimal Value") {
+                    this.rightAscDecimal = parseFloat(this.rightAscDecimal) + 1;
+                }
+                else if(this.mode === "Right Ascension by Hours and Minutes") {
+                    this.rightAscMinutes = parseInt(this.rightAscMinutes) + 1;
+                    if(this.rightAscMinutes >= 60) {
+                        this.rightAscHours = parseInt(this.rightAscHours) + 1;
+                        this.rightAscMinutes = parseInt(this.rightAscMinutes)  - 60;
+                    }
+                }
+            } else if(val == 5) {
+                // Azimuth Neg
+                if(this.mode === "Azimuth and Elevation") {
+                    this.azimuth = parseFloat(this.azimuth) - 1;
+                }
+            } else if(val == 6) {
+                // Elevation Pos
+                if(this.mode === "Azimuth and Elevation") {
+                    this.elevation = parseFloat(this.elevation) + 1;
+                }
+            } else if(val == 7) {
+                // Elevation Neg
+                if(this.mode === "Azimuth and Elevation") {
+                    this.elevation = parseFloat(this.elevation) - 1;
+                }
+            } else if(val == 8) {
+                // Azimuth Pos
+                if(this.mode === "Azimuth and Elevation") {
+                    this.azimuth = parseFloat(this.azimuth) + 1;
+                }
+            }
+
+
+            this.updateRightAsc();
         },
         submit(){
             // This method will use a TCP client to send commands to the telescope.
         }
-        // Considering giving each button a seperate method, but also considering cutting down on the number of buttons
     },
     components: {
         Loading
